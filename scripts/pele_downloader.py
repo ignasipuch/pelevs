126fb4f01fc7eac427a4a548f007038b6eba704f
Ignasi Puch-Giner <ignasi.puchginer@bsc.es>
2023-06-25 18:26:18 +0200

    First functional pele_downloader script to download the data from a whole simulation.

--- scripts/pele_downloader.py
+++ scripts/pele_downloader.py
@@ -12,6 +12,7 @@ import sys
 import argparse
 import os
 import shutil
+import pandas as pd
 
 def parse_args(args):
     """
@@ -32,7 +33,7 @@ def parse_args(args):
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-o", "--ouput_name", type=list, dest="output_name",
+    parser.add_argument("-o", "--ouput_name", type=str, dest="output_name",
                         default=None, help="Name of the folder to be created.")
     
     parsed_args = parser.parse_args(args)
@@ -40,37 +41,180 @@ def parse_args(args):
     return parsed_args
 
 def pele_reports_retriever(output_name):
-    
-    def folder_hierarchy(output_name):
+    """
+    Function
+    ----------
+    Retrieves all the reports done in all the simulations belonging to single
+    pele_simulation folder.
 
-        source_directory = 'pele_simulation'
-        destination_directory = output_name
+    Parameters
+    ----------
+    - output_name : str
+        Name of the folder where the data is going to be stored.
+    """   
+
+    def folders_with_simulations(output_name):
+        """
+        Function
+        ----------
+        Stores all the directory trees that have at some point a 
+        rescoring method name: xshort, short, long or xlong. It stores 
+        them in a variable named folders_to_check.
+
+        Parameters
+        ----------
+        - output_name : str
+            Name of the folder where the data is going to be stored.
+
+        Returns
+        ----------
+        - folders_to_check : list
+            List of paths that are going to contain simulations of the 
+            whole dataset.
+        """  
 
         if not os.path.isdir(output_name):
             os.mkdir(output_name)
 
-        shutil.copytree(source_directory, destination_directory)
-
-    def folders_with_simulations():
-
         folders_to_check = []
+        rescorings = ['xshort','short','long','xlong']
+        
+        for root, _, _ in os.walk('pele_simulation/'):
 
-        rescoring_methods = ['xshort','short','long','xlong']
-        for direct in os.walk('4_pele_simulation/'):
-            if os.path.basename(direct[0]) in rescoring_methods:
-                folders_to_check.append(direct[0])
+            if os.path.basename(root) in rescorings:
+                new_path = root.replace('pele_simulation', output_name)
+                folders_to_check.append(root)
+                
+                if not os.path.isdir(new_path):
+                    os.mkdir(new_path)
 
         return folders_to_check
 
-    folder_hierarchy(output_name)
-    folders_to_check = folders_with_simulations()
+    def dataset_retriever(output_name, folders_to_check):
+        """
+        Function
+        ----------
+        Searches all the directories inputed with the list folders_to_check
+        and copies all the reports into the newly created folder to download.
 
+        Parameters
+        ----------
+        - output_name : str
+            Name of the folder where the data is going to be stored.
+        - folders_to_check : list
+            List of paths that are going to contain simulations of the 
+            whole dataset.
+
+        Returns
+        ----------
+        - failed_simulations : list
+            List of simulation's paths where the simulations have failed.
+        """  
+
+        def simulation_retriever(output_name, simulation_path):
+            """
+            Function
+            ----------
+            Copies all the reports into the wanted directories and generates 
+            the failed simulations_list.
+
+            Parameters
+            ----------
+            - output_name : str
+                Name of the folder where the data is going to be stored.
+            - simulation_path : str
+                System folder to check for simulations and with that reports.
+
+            Returns
+            ----------
+            - rescoring_path : str
+                Path of that is being checked.
+            - system : str  
+                System that is being checked.
+            - failed_bool : bool
+                Boolean indicating whether the simulation has failed or not.
+            """  
+
+            system = simulation_path.split('/')[-1]
+            rescoring_path = '/'.join(simulation_path.split('/')[:-1])
+            nbd_suite_path = '6_adaptive_pele_simulation/complex_2/output'
+            ligand_folder = simulation_path.replace('pele_simulation', output_name)
+
+            whole_path = os.path.join(simulation_path,system,nbd_suite_path)
+            
+            if os.path.isdir(whole_path):
+
+                failed_bool = False
+
+                for epoch in [x for x in os.listdir(whole_path) if x.isdigit()]:
+
+                    epoch_path = os.path.join(whole_path,epoch) 
+                    new_epoch_path = os.path.join(ligand_folder,epoch)
+                    
+                    if not os.path.isdir(new_epoch_path):
+                        os.mkdir(new_epoch_path)
+
+                    for report in [x for x in os.listdir(epoch_path) if x.startswith('report')]:
+                        report_path = os.path.join(epoch_path,report)
+                        
+                        shutil.copy(report_path,new_epoch_path)
+
+            else: failed_bool = True
+
+            return rescoring_path, system, failed_bool
+
+        failed_simulations = []
+               
+        for folder in folders_to_check:
+            for simulation in os.listdir(folder):
+                simulation_path = os.path.join(folder,simulation)
+
+                if os.path.isdir(simulation_path):
+                    ligand_folder = simulation_path.replace('pele_simulation', output_name)
+
+                    if not os.path.isdir(ligand_folder):
+                        os.mkdir(ligand_folder)
+                        
+                    rescoring_path, system, failed_bool = simulation_retriever(output_name, simulation_path) 
+                    
+                    if failed_bool:
+                        failed_simulations.append(os.path.join(rescoring_path,system))
+        
+        print(' - All report files copied successfully.')
+
+        return failed_simulations
+
+    def failed_simulations_writer(output_name, failed_simulations):
+        """
+        Function
+        ----------
+        Searches all the directories inputed with the list folders_to_check
+        and copies all the reports into the newly created folder to download.
+
+        Parameters
+        ----------
+        - output_name : str
+            Name of the folder where the data is going to be stored.
+        - failed_simulations : list
+            List of paths that where the simulation has failed.
+        """  
+
+        csv_path = os.path.join(output_name,'failed_simulations.csv')
+        df = pd.DataFrame()
+        df['failed_simulations'] = failed_simulations
+        df.to_csv(csv_path, index=False)
+
+        print(' - Total number of failed simulations: {}.'.format(len(failed_simulations)))
+
+    folders_to_check = folders_with_simulations(output_name)
+    failed_simulations = dataset_retriever(output_name, folders_to_check)
+    failed_simulations_writer(output_name, failed_simulations)
     
 def main(args):
     """
     Function
     ----------
-    It reads the command-line arguments and runs lice_results.
+    It reads the command-line arguments and runs pele_reports_retriever.
 
     Parameters
     ----------
