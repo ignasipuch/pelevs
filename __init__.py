6047f840c84df05215a79a0697370b68f6ca62fd
Ignasi Puch-Giner <ignasi.puchginer@bsc.es>
2023-07-07 11:19:27 +0200

    Update

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from .inputPrepare import InputPreparation
 from .dockingJob import DockingJob
 from .dockingAnalysis import DockingAnalyzer
 from .peleJob import PELE
+from .peleAnalysis import PELEAnalyzer
