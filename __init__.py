162a2d2f29e14fe2e623d619f6b09aa2b0467e61
Ignasi Puch-Giner <ignasi.puchginer@bsc.es>
2023-11-03 11:03:13 +0100

    Moving scripts into folder

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .inputPrepare import InputPreparation
-from .dockingJob import DockingJob
-from .dockingAnalysis import DockingAnalyzer
-from .peleJob import PELE
-from .peleAnalysis import PELEAnalyzer
